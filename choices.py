d9b77fa5fabe0211690e69def2e8c995657588e9
Sylvain Leveugle <sylvain.leveugle@gmail.com>
2023-02-05 21:40:15 +0000

    correct choice missing coma

--- choices.py
+++ choices.py
@@ -1,4 +1,4 @@
 choices_music = (
-    "we_are_the_champions", "allumer_le_feu", "bella_ciao", "heygh_ho", "bob_l_eponge","another_brick_in_the_wall"
+    "we_are_the_champions", "allumer_le_feu", "bella_ciao", "heygh_ho", "bob_l_eponge","another_brick_in_the_wall",
     "pirate_caribeean", "i_was_made_for_loving_you", "zouk_machine", "marche_empereur", "lady_marmelade")
 
