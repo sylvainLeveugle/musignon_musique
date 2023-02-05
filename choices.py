cb3a63ab8c85c1412fef0fdee8c9eb2bab0e4a98
Sylvain Leveugle <sylvain.leveugle@gmail.com>
2023-02-05 21:37:19 +0000

    Update musics 6 instrus

--- choices.py
+++ choices.py
@@ -1,10 +1,4 @@
 choices_music = (
-    "we_are_the_champions", "allumer_le_feu", "bella_ciao", "heygh_ho", "bob_l_eponge",
+    "we_are_the_champions", "allumer_le_feu", "bella_ciao", "heygh_ho", "bob_l_eponge","another_brick_in_the_wall"
     "pirate_caribeean", "i_was_made_for_loving_you", "zouk_machine", "marche_empereur", "lady_marmelade")
-choices_musignon_1 = (1, 9, 17, 25, 28, 42, 57, 59, 66, 69, 76, 104)
-choices_musignon_2 = (1, 9, 17, 25, 28, 42, 57, 59, 66, 69, 76, 104)
-choices_musignon_3 = (1, 9, 17, 25, 28, 42, 57, 59, 66, 69, 76, 104)
-min_rythm = -7
-max_rythm = 7
-min_note_change = -16
-max_note_change = 16
+
